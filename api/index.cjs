ab50c53600b7448a191f74f9d5e041ba7ec394e9
AmrSaher <amrosaher1234@gmail.com>
2024-11-05 02:56:26 +0200

    try#13(vercel)

--- api/index.cjs
+++ api/index.cjs
@@ -1,11 +1,18 @@
 // index.cjs
 
-// Import the app from app.mjs using dynamic import
-const path = require('path');
+// Use dynamic import to load the ES module
+const path = require("path");
 
 (async () => {
-    const { default: app } = await import(path.resolve(__dirname, '../app.mjs'));
+    try {
+        const { default: app } = await import(
+            path.resolve(__dirname, "../app.mjs")
+        );
 
-    // Export the app as the default export
-    module.exports = app;
+        // Export the app
+        module.exports = app;
+    } catch (error) {
+        console.error("Failed to import app:", error);
+        process.exit(1); // Exit the process with a failure status
+    }
 })();

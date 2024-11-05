8aa4dd9679fc464738c0d92ea83fa82b4bcc348f
AmrSaher <amrosaher1234@gmail.com>
2024-11-05 02:54:36 +0200

    try#12(vercel)

--- api/index.cjs
+++ api/index.cjs
@@ -1,5 +1,11 @@
-let app;
+// index.cjs
+
+// Import the app from app.mjs using dynamic import
+const path = require('path');
+
 (async () => {
-    app = await import("../app.mjs");
+    const { default: app } = await import(path.resolve(__dirname, '../app.mjs'));
+
+    // Export the app as the default export
+    module.exports = app;
 })();
-export default app;
